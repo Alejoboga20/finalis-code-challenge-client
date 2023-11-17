b7fb140c93eabd817ffcdee3022d98428df1066f
Alejo Boga <60139228+Alejoboga20@users.noreply.github.com>
2023-11-17 13:03:38 -0500

    Routing (#2)
    * Add react-router-dom dependency
    
    * Add router configuration
    
    * Remove App component and add RouterProvider
    
    * create base pages
    
    * implement lazy loading
    
    * add suspense for the lazy loaded components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './router/router.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<Suspense fallback={<div>Loading...</div>}>
+			<RouterProvider router={router} />
+		</Suspense>
 	</React.StrictMode>
 );
---
+++ src/router/router.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react-refresh/only-export-components */
+import { lazy } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../forms/pages/HomePage'));
+const NewPage = lazy(() => import('../forms/pages/NewPage'));
+const FormPage = lazy(() => import('../forms/pages/FormPage'));
+
+enum Routes {
+	HOME = '/',
+	NEW = '/new',
+	FORM = '/form/:id',
+}
+
+export const router = createBrowserRouter([
+	{
+		path: Routes.HOME,
+		element: <HomePage />,
+	},
+	{
+		path: Routes.NEW,
+		element: <NewPage />,
+	},
+	{
+		path: Routes.FORM,
+		element: <FormPage />,
+	},
+]);
