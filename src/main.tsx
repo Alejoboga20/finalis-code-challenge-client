bda1be3fc75c55fc702222d3ab20234653f3be26
Alejo Boga <alejoboga20@gmail.com>
2023-11-17 16:43:10 -0300

    Add Spinner component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router/router.tsx';
+import { Spinner } from './common/components/Spinner.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Suspense fallback={<div>Loading...</div>}>
+		<Suspense fallback={<Spinner />}>
 			<RouterProvider router={router} />
 		</Suspense>
 	</React.StrictMode>
